112c550f2c160a77a6cf337b087f7c7325e35a2a
astrid-ridg <astrid0888@gmail.com>
2024-03-03 16:55:50 +0200

    JS: import API search, create default search value
    Import API link to fetch weather with adjustment to searchCity function; create default weather search for Cape Town

--- src/script.js
+++ src/script.js
@@ -1,13 +1,24 @@
+function displayTemp(response) {
+  let temperatureDisplay = document.querySelector(".card-text-main");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityDisplay = document.querySelector(".card-title-main");
+  cityDisplay.innerHTML = response.data.city;
+  temperatureDisplay.innerHTML = `${temperature}°C`;
+}
+
+function fetchWeather(city) {
+  let apiKey = "fad3604f59545d05634atobc1ad582a8";
+  let units = "metric";
+  let weatherAPI = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+  axios.get(weatherAPI).then(displayTemp);
+}
+
 function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-city");
-  let cityDisplay = document.querySelector(".card-title-main");
-  if (searchInput.value) {
-    cityDisplay.innerHTML = `${searchInput.value}`;
-  } else {
-    cityDisplay.innerHTML = null;
-    alert("Please type a city");
-  }
+  fetchWeather(searchInput.value);
 }
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchCity);
+
+fetchWeather("Cape Town");
