6d22a33d0d4617d58a009e1dece22517b139d978
astrid-ridg <astrid0888@gmail.com>
2024-03-03 18:18:17 +0200

    JS: import weather details and date function
    Note metric units and display

--- src/script.js
+++ src/script.js
@@ -2,8 +2,40 @@ function displayTemp(response) {
   let temperatureDisplay = document.querySelector(".card-text-main");
   let temperature = Math.round(response.data.temperature.current);
   let cityDisplay = document.querySelector(".card-title-main");
+  let descriptionDisplay = document.querySelector("#description");
+  let windSpeedDisplay = document.querySelector("#wind-speed");
+  let humidityDisplay = document.querySelector("#humidity");
+  let timeDisplay = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
   cityDisplay.innerHTML = response.data.city;
   temperatureDisplay.innerHTML = `${temperature}°C`;
+  descriptionDisplay.innerHTML = response.data.condition.description;
+  windSpeedDisplay.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityDisplay.innerHTML = `${response.data.temperature.humidity}%`;
+  timeDisplay.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function fetchWeather(city) {
